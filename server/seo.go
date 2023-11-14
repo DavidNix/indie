64a63a4693bf0df0c7a30b0bd7d376137c120037
David Nix <hello@davidnix.io>
2023-11-13 21:23:05 -0700

    Implement simple sitemap

--- server/seo.go
+++ server/seo.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	_ "embed"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 
@@ -19,4 +20,28 @@ func robotsHandler(c echo.Context) error {
 	return c.String(http.StatusOK, fmt.Sprintf(robotsTxt, baseURL))
 }
 
-func sitemapHandler(c echo.Context) error { return c.XML(http.StatusOK, nil) }
+// See: https://www.sitemaps.org/protocol.html
+
+type URLSet struct {
+	XMLName xml.Name `xml:"urlset"`
+	XMLNS   string   `xml:"xmlns,attr"`
+	URLs    []URL    `xml:"url"`
+}
+
+type URL struct {
+	XMLName    xml.Name `xml:"url"`
+	Loc        string   `xml:"loc"`
+	LastMod    string   `xml:"lastmod,omitempty"`
+	ChangeFreq string   `xml:"changefreq,omitempty"`
+	Priority   float64  `xml:"priority,omitempty"`
+}
+
+func sitemapHandler(c echo.Context) error {
+	set := URLSet{
+		XMLNS: "http://www.sitemaps.org/schemas/sitemap/0.9",
+		URLs: []URL{
+			{Loc: baseURL + "/users"},
+		},
+	}
+	return c.XML(http.StatusOK, set)
+}
