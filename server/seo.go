fdf9528473dda0ac55f04bcf920b6370afba915a
David Nix <hello@davidnix.io>
2023-11-13 20:54:43 -0700

    Dynamic robots.txt

--- server/seo.go
+++ server/seo.go
@@ -2,15 +2,21 @@ package server
 
 import (
 	_ "embed"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-// TODO: Change the sitemap URL.
-//go:embed views/robots.txt
-var robotstxt string
+const baseURL = "https://CHANGEME.example.com"
 
-func robotsHandler(c echo.Context) error { return c.String(http.StatusOK, robotstxt) }
+const robotsTxt = `User-agent: *
+Allow: /
+Sitemap: %s/sitemap.xml
+`
 
-func sitemapHandler(c echo.Context) error { return c.String(http.StatusOK, "TODO: Sitemap") }
+func robotsHandler(c echo.Context) error {
+	return c.String(http.StatusOK, fmt.Sprintf(robotsTxt, baseURL))
+}
+
+func sitemapHandler(c echo.Context) error { return c.XML(http.StatusOK, nil) }
