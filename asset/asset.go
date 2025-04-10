f8501e3d098483f3b5223328639847d30244691f
David Nix <hello@davidnix.io>
2025-04-10 07:13:33 -0600

    image src

--- asset/asset.go
+++ asset/asset.go
@@ -1,1 +1,9 @@
 package asset
+
+var cacheKey string
+
+// SetCacheKey sets a cache busting key that will be appended to asset URLs.
+// This helps force browsers to download fresh copies of assets when the key changes.
+func SetCacheKey(key string) {
+	cacheKey = key
+}
--- asset/image.go
+++ asset/image.go
@@ -1,3 +1,17 @@
 package asset
 
-func ImageSrc() {}
+import "strings"
+
+// ImageSrc appends a cache busting key to the given image path URL.
+// If a cache key is not set, it returns the original image path unchanged.
+// If the image path already contains query parameters, the cache key is appended with &.
+// Otherwise, the cache key is appended with ?.
+func ImageSrc(imgPath string) string {
+	if cacheKey == "" {
+		return imgPath
+	}
+	if i := strings.Index(imgPath, "?"); i != -1 {
+		return imgPath + "&v=" + cacheKey
+	}
+	return imgPath + "?v=" + cacheKey
+}
