d65b6c701c054f049816f7ce76a0664b8703d5db
David Nix <hello@davidnix.io>
2025-04-11 11:42:43 -0600

    don't log function address

--- cmd/indie/server_cmd.go
+++ cmd/indie/server_cmd.go
@@ -40,7 +40,7 @@ func runServer(cmd *cobra.Command, args []string) error {
 	var eg errgroup.Group
 
 	eg.Go(func() error {
-		slog.Info("Starting server", "address", srv.Addr, "version", version.Version)
+		slog.Info("Starting server", "address", srv.Addr, "version", version.Version())
 		return srv.ListenAndServe()
 	})
 
